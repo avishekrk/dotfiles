800e14637540ccaa16b60f380d9459c69324814b
Avishek Kumar <avishek.kumar@outlook.com>
2019-10-13 18:06:36 -0500

    added installation script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#install script for dotfiles
+
+#emacs:just copy the init.el files to the right folder
+
+PRELUDE_DIR="${HOME}/.emacs.d/personal/"
+EMACS_DIR="${HOME}/.emacs.d/"
+
+if [ -d ${PRELUDE_DIR} ]
+then
+    echo "${PRELUDE_DIR} exists installing inti.el there:"
+    cp -v init.el ${PRELUDE_DIR}
+elif [ -d ${EMACS_DIR} ]
+then
+    echo "${EMACS_DIR} exists installing init.el there:"
+    cp -v init.el ${EMACS_DIR}
+else
+    echo "Neither ${PRELUDE_DIR} or #{EMACS_DIR} exists making ${EMACS_DIR}"
+    #mkdir -vp ${EMACS_DIR}
+    cp -v init.el ${EMACS_DIR}
+fi
