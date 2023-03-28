9254bc67208218e23b1dcf6ae04ad93f7f39aeed
Avishek Kumar <Avishek_Kumar@intuit.com>
2023-03-28 09:17:53 -0700

    added markdown mode to emacs

--- emacs/init.el
+++ emacs/init.el
@@ -1,38 +1,5 @@
-;; Configure package.el to include MELPA.
-(require 'package)
-(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(package-initialize)
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "emacs-init.org"
+                   user-emacs-directory))
 
-
-;; Ensure that use-package is installed.
-;;
-;; If use-package isn't already installed, it's extremely likely that this is a
-;; fresh installation! So we'll want to update the package repository and
-;; install use-package before loading the literate configuration.
-(when (not (package-installed-p 'use-package))
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(org-babel-load-file "~/.emacs.d/emacs-config.org")
-(org-babel-load-file "~/.emacs.d/emacs-python.org")
-(org-babel-load-file "~/.emacs.d/emacs-ui.org")
-(org-babel-load-file "~/.emacs.d/emacs-files.org")
-(org-babel-load-file "~/.emacs.d/emacs-org.org")
-(org-babel-load-file "~/.emacs.d/emacs-sql.org")
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(jedi company-jedi company-anaconda anaconda-mode blacken pytest use-package auto-compile)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
